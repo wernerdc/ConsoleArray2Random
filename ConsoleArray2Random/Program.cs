e99625d9bfc54b5b6ca7fa9ef85e9dcd8a7a55f8
wernerdc <manuel.werner3@gmx.de>
2024-07-23 15:18:27 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleArray2Random/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleArray2Random {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Clear();
+            Console.WriteLine("ConsoleArray2Random\n");
+
+            Random random = new Random();
+            int[,] randomArray = new int[10,14];
+            for (int i = 0; i < randomArray.GetLength(0); i++) {
+                for (int j = 0; j < randomArray.GetLength(1) - 4; j++) {
+                    randomArray[i, j] = random.Next(100);
+                }
+            }
+
+            for (int i = 0; i < randomArray.GetLength(0); i++) {
+                for (int j = 0; j < randomArray.GetLength(1); j++) {
+                    Console.Write(randomArray[i, j] + " ");
+                }
+                Console.Write("\n");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
