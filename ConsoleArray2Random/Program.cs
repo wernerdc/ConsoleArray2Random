209f04df38841cd5dfa88ab5a431efe8cdb51c1e
wernerdc <manuel.werner3@gmx.de>
2024-07-23 21:26:39 +0200

    Final version 1.0

--- ConsoleArray2Random/Program.cs
+++ ConsoleArray2Random/Program.cs
@@ -1,39 +1,82 @@
 ﻿namespace ConsoleArray2Random {
+
     internal class Program {
+
         static void Main(string[] args) {
+
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Gray;
             Console.Clear();
             Console.WriteLine("ConsoleArray2Random\n");
 
+            Console.WriteLine("2-Dimensionales Array int[10,14] anlegen\n" +
+                              "zufällige Werte ins Array schreiben\n" +
+                              "Werte des Arrays anzeigen\n");
+
+            int[,] randomArray = CreateAndShowRandomArray();
+
+            Console.WriteLine("Summe, Mittelwert, Min und Max berechnen\n" +
+                              "Werte des Array anzeigen\n");
+
+            CalcAndShowRandomArray(randomArray);
+
+            Console.WriteLine("\nPress enter to exit...");
+            Console.ReadLine();
+        }
+
+        private static int[,] CreateAndShowRandomArray() {
+            
             Random random = new Random();
-            int[,] randomArray = new int[10,14];
+            int[,] randomArray = new int[10, 14];
+
+            for (int i = 0; i < randomArray.GetLength(0); i++) {
+                for (int j = 0; j < randomArray.GetLength(1) - 4; j++) {
+
+                    randomArray[i, j] = random.Next(100);
+                    //Console.Write(string.Format("{0,3}", randomArray[i, j]));
+                    // = same as:
+                    Console.Write("{0,4}", randomArray[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            return randomArray;
+        }
+
+        private static void CalcAndShowRandomArray(int[,] randomArray) {
             
             for (int i = 0; i < randomArray.GetLength(0); i++) {
-                for (int j = 0; j < randomArray.GetLength(1); j++) {
-                    
-                    if (j < randomArray.GetLength(1) - 4) {
-                        randomArray[i, j] = random.Next(100);
-                        Console.Write(string.Format("{0,3}", randomArray[i, j]));
-                    
-                    } else if (j == randomArray.GetLength(1) -1) {
-                        //randomArray[i, j -4] = 
+                int sum = 0;
+                int min = 99;
+                int max = 0;
+
+                for (int j = 0; j < randomArray.GetLength(1) - 4; j++) {
+                    Console.Write("{0,4}", randomArray[i, j]);
+
+                    sum += randomArray[i, j];
+                    if (min > randomArray[i, j]) {
+                        min = randomArray[i, j];
                     }
-
-                    
-                }
-            }
-
-            for (int i = 0; i < randomArray.GetLength(0); i++) {
-                for (int j = 0; j < randomArray.GetLength(1); j++) {
-                    Console.Write(string.Format("{0,3}", randomArray[i, j]));
+                    if (max < randomArray[i, j]) {
+                        max = randomArray[i, j];
+                    }
                 }
 
+                randomArray[i, 10] = sum;
+                randomArray[i, 11] = sum / 10;
+                randomArray[i, 12] = min;
+                randomArray[i, 13] = max;
 
-                Console.Write("\n");
+                // print calculations
+                Console.Write(" ");
+                Console.ForegroundColor = ConsoleColor.Black;
+                for (int j = 10; j < 14; j++) {
+                    Console.Write("{0,4}", randomArray[i, j]);
+                }
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine();
             }
-
-            Console.ReadLine();
         }
     }
 }
